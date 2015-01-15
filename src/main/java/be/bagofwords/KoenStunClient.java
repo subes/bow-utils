bf31c03b58622f354b54e66a15b173657d027d01
Koen Deschacht <koendeschacht@gmail.com>
2015-01-15 10:30:41 +0100

    Use UDP to determine remote port

--- src/main/java/be/bagofwords/KoenStunClient.java
+++ src/main/java/be/bagofwords/KoenStunClient.java
@@ -1,13 +1,15 @@
 package be.bagofwords;
 
 import be.bagofwords.ui.UI;
-import be.bagofwords.util.WrappedSocketConnection;
+import org.apache.commons.io.IOUtils;
 
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
-import java.net.Socket;
 import java.util.Random;
 
 /**
@@ -18,45 +20,90 @@ public class KoenStunClient {
     private static final int my_local_port = 9055;
 
     public static void main(String[] args) throws IOException {
-        Socket stunSocket = new Socket(args[0], Integer.parseInt(args[1]), null, my_local_port);
-        WrappedSocketConnection wrappedStunSocket = new WrappedSocketConnection(stunSocket);
-        String myRemoteAddress = wrappedStunSocket.readString();
-        int myRemotePort = wrappedStunSocket.readInt();
+        DatagramSocket socket = new DatagramSocket(my_local_port);
+        Random random = new Random();
+        byte[] myData = new byte[]{(byte) random.nextInt(127)};
+        {
+            UI.write("Sending UDP packet to calvin server");
+            StunInfo stunInfo = getStunInfo(socket, "calvin.x-spell.be", Integer.parseInt(args[1]));
+            String myRemoteAddress = stunInfo.getAddress();
+            int myRemotePort = stunInfo.getPort();
+            UI.write("My remote address is " + myRemoteAddress + " and port is " + myRemotePort);
+        }
+
+        UI.write("Sending UDP packet to stun server");
+        StunInfo stunInfo = getStunInfo(socket, args[0], Integer.parseInt(args[1]));
+        String myRemoteAddress = stunInfo.getAddress();
+        int myRemotePort = stunInfo.getPort();
         UI.write("My remote address is " + myRemoteAddress + " and port is " + myRemotePort);
-        wrappedStunSocket.writeBoolean(true);
-        wrappedStunSocket.flush();
-        wrappedStunSocket.close();
 
         String otherRemoteAddress = UI.read("Please give the address of the other client");
         int otherRemotePort = UI.readInt("Please give the port of the other client");
         InetAddress otherAddress = InetAddress.getByName(otherRemoteAddress);
 
-        Random random = new Random();
-        byte[] myData = new byte[]{(byte) random.nextInt(127)};
         byte[] receivedData = new byte[1];
 
         UI.write("Will try to send my data to the other client. Hold on your bretels!");
-        int numOfSuccesfulReceived = 0;
-        DatagramSocket outgoingSocket = new DatagramSocket(my_local_port);
+        int numOfSuccessfullyReceived = 0;
+
         while (true) {
             UI.write("Sending " + myData[0] + " to " + otherRemoteAddress + ":" + otherRemotePort);
-            outgoingSocket.send(new DatagramPacket(myData, myData.length, otherAddress, otherRemotePort));
+            socket.send(new DatagramPacket(myData, myData.length, otherAddress, otherRemotePort));
             int timeout = 500 + random.nextInt(1000);
-            outgoingSocket.setSoTimeout(timeout);
+            socket.setSoTimeout(timeout);
             UI.write("Trying to receive data from " + otherRemoteAddress + ":" + otherRemotePort + " timout=" + timeout);
             try {
                 DatagramPacket receivingPacket = new DatagramPacket(receivedData, receivedData.length);
-                outgoingSocket.receive(receivingPacket);
+                socket.receive(receivingPacket);
                 byte[] actualReceivedData = receivingPacket.getData();
                 if (actualReceivedData.length != 1) {
                     UI.write("Received " + actualReceivedData.length + " bytes?");
                 } else {
-                    numOfSuccesfulReceived++;
-                    UI.write("Received data from other client! " + actualReceivedData[0] + "(already " + numOfSuccesfulReceived + " times)");
+                    numOfSuccessfullyReceived++;
+                    UI.write("Received data from other client! " + actualReceivedData[0] + "(already " + numOfSuccessfullyReceived + " times)");
                 }
             } catch (Exception e) {
                 UI.writeError("Received error while trying to receive data", e);
             }
+        }
+    }
+
+    private static StunInfo getStunInfo(DatagramSocket socket, String stunServerAddress, int stunServerPort) throws IOException {
+        socket.setSoTimeout(1000);
+        while (true) {
+            byte[] myData = {1};
+            socket.send(new DatagramPacket(myData, myData.length, InetAddress.getByName(stunServerAddress), stunServerPort));
+            try {
+                byte[] stunData = new byte[100];
+                DatagramPacket receivePacket = new DatagramPacket(stunData, stunData.length);
+                socket.receive(receivePacket);
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(new ByteArrayInputStream(stunData)));
+                String myRemoteAddress = bufferedReader.readLine();
+                int myRemotePort = Integer.parseInt(bufferedReader.readLine());
+                IOUtils.closeQuietly(bufferedReader);
+                return new StunInfo(myRemoteAddress, myRemotePort);
+            } catch (Exception exp) {
+                UI.writeError("Received exception while trying to get data from stun server " + exp.getMessage());
+            }
+        }
+
+    }
+
+    private static class StunInfo {
+        private String address;
+        private int port;
+
+        public StunInfo(String address, int port) {
+            this.address = address;
+            this.port = port;
+        }
+
+        public String getAddress() {
+            return address;
+        }
+
+        public int getPort() {
+            return port;
         }
     }
 
--- src/main/java/be/bagofwords/KoenStunServer.java
+++ src/main/java/be/bagofwords/KoenStunServer.java
@@ -1,12 +1,14 @@
 package be.bagofwords;
 
 import be.bagofwords.ui.UI;
-import be.bagofwords.util.WrappedSocketConnection;
 
+import java.io.BufferedWriter;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
 import java.net.InetAddress;
-import java.net.ServerSocket;
-import java.net.Socket;
 
 /**
  * Created by Koen Deschacht (koendeschacht@gmail.com) on 14/01/15.
@@ -14,17 +16,29 @@ import java.net.Socket;
 public class KoenStunServer {
 
     public static void main(String[] args) throws IOException {
-        ServerSocket serverSocket = new ServerSocket(Integer.parseInt(args[0]));
+        DatagramSocket socket = new DatagramSocket(Integer.parseInt(args[0]));
         while (true) {
-            Socket clientSocket = serverSocket.accept();
-            WrappedSocketConnection wrappedSocketConnection = new WrappedSocketConnection(clientSocket);
-            InetAddress inetAddress = clientSocket.getInetAddress();
-            UI.write("Client address=" + inetAddress.getHostAddress() + " port=" + clientSocket.getPort() + " local_port=" + clientSocket.getLocalPort());
-            wrappedSocketConnection.writeString(inetAddress.getHostAddress());
-            wrappedSocketConnection.writeInt(clientSocket.getPort());
-            wrappedSocketConnection.flush();
-            wrappedSocketConnection.readBoolean();
-            wrappedSocketConnection.close();
+            try {
+                byte[] stunData = new byte[100];
+                DatagramPacket receivePacket = new DatagramPacket(stunData, stunData.length);
+                socket.receive(receivePacket);
+                InetAddress clientInetAddress = receivePacket.getAddress();
+                String clientAddress = clientInetAddress.getHostAddress();
+                int clientPort = receivePacket.getPort();
+                UI.write("Client address=" + clientAddress + " port=" + clientPort);
+                ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+                BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(byteArrayOutputStream));
+                writer.write(clientAddress);
+                writer.write("\n");
+                writer.write(Integer.toString(clientPort));
+                writer.write("\n");
+                writer.close();
+                byte[] clientData = byteArrayOutputStream.toByteArray();
+                DatagramPacket sendPacket = new DatagramPacket(clientData, 0, clientData.length, clientInetAddress, clientPort);
+                socket.send(sendPacket);
+            } catch (Exception exp) {
+                UI.writeError("Received exception", exp);
+            }
         }
     }
 
