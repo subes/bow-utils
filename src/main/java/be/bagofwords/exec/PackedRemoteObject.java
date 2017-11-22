64902fd8d10096294d788a09f5f53856d765b7c9
Koen Deschacht <koendeschacht@gmail.com>
2017-11-22 14:31:02 +0100

    Added equals and hashCode methods to PackedRemoteObject

--- src/main/java/be/bagofwords/exec/PackedRemoteObject.java
+++ src/main/java/be/bagofwords/exec/PackedRemoteObject.java
@@ -2,6 +2,7 @@ package be.bagofwords.exec;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import java.util.Arrays;
 import java.util.Map;
 
 /**
@@ -19,5 +20,25 @@ public class PackedRemoteObject {
         this.objectClassName = objectClassName;
         this.serializedObject = serializedObject;
         this.classSources = classSources;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        PackedRemoteObject that = (PackedRemoteObject) o;
+
+        if (!objectClassName.equals(that.objectClassName)) return false;
+        if (!Arrays.equals(serializedObject, that.serializedObject)) return false;
+        return classSources.equals(that.classSources);
+    }
+
+    @Override
+    public synchronized int hashCode() {
+        int hash = objectClassName.hashCode();
+        hash = 31 * hash + Arrays.hashCode(serializedObject);
+        hash = 31 * hash + classSources.hashCode();
+        return hash;
     }
 }
